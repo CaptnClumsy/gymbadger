d52b63eee5dbd179ad3180e01a7fdc75c4e01fc3
Peter Raymond <PRaymond@serena.com>
2018-02-02 14:40:36 +0000

    Add fb app id to meta tags and mark some gyms as no longer being parks

---
+++ src/main/resources/db/changelogs/gym-data-updates-02022018.sql
@@ -0,0 +1,8 @@
+BEGIN;
+UPDATE gyms set park=false WHERE name='Nomansland Model Aircraft Landing Strip';
+UPDATE gyms set park=false WHERE name='St Albans North Town Marker';
+UPDATE gyms set park=false WHERE name='Verulam Consultants';
+UPDATE gyms set park=false WHERE name='St. Albans Rugby Club';
+UPDATE gyms set park=false WHERE name='Sopwell Nunnery';
+UPDATE gyms set park=false WHERE name='Leoro Gallery';
+COMMIT;
