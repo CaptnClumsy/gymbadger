dc60b2e2bc4e2c4eac62831a59c418aa354ef76c
petray <petray@Hawk>
2018-03-17 10:29:23 +0000

    Missing gym in harpenden and more raid bosses

--- src/main/resources/db/changelogs/pokemon-updates-16032018.sql
+++ src/main/resources/db/changelogs/pokemon-updates-16032018.sql
@@ -1,3 +1,8 @@
 begin;
 insert into pokemon (id, name, raidboss) values (nextval('pokemonids'), 'Manectric', true);
+insert into pokemon (id, name, raidboss) values (nextval('pokemonids'), 'Electabuzz', true);
+insert into pokemon (id, name, raidboss) values (nextval('pokemonids'), 'Jolteon', true);
+
+insert into gyms (id, name, lat, long, park, areaid, image_url) values (nextval('gymids'), 'Waste Awareness Mural', 51.807341, -0.339049, false, (SELECT id FROM areas WHERE name='Harpenden'), 'http://lh6.ggpht.com/RHiuFqYfdl-dQ-Kn-vnFRTOvmyjc2WE5MsXI10-yNDRxO_QesRQIus1EPm-312cmO45A9VS4K-orD88j-iQr');
+
 commit;
