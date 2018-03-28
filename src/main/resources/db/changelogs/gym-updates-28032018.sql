8d0c84c5115327da70ad8bdc213c42e9138be014
petray <petray@Hawk>
2018-03-28 17:07:50 +0100

    Move location of new gym a little bit

--- src/main/resources/db/changelogs/gym-updates-28032018.sql
+++ src/main/resources/db/changelogs/gym-updates-28032018.sql
@@ -1,5 +1,5 @@
 begin;
 update gyms set deleted=true where name='Colney Heath Community Park';
 insert into gyms (id, name, lat, long, park, areaid, image_url) values (nextval('gymids'), 'Colney Heath Community Park', 51.741881, -0.267596, false, (SELECT id FROM areas WHERE name='Colney Heath'), 'http://lh5.ggpht.com/ulbgIf2AoHScIPMSpqwFxz-HbWSjx14Jo-Tmv29Ra0aeks11AULH1ht3RAUWncId25BxcUCvBwv4F-u7PkeB2Q');
-insert into gyms (id, name, lat, long, park, areaid, image_url) values (nextval('gymids'), 'Woodbury Field Entry Marker', 51.709302, -0.363664, false, (SELECT id FROM areas WHERE name='Bricket Wood'), null);
+insert into gyms (id, name, lat, long, park, areaid, image_url) values (nextval('gymids'), 'Woodbury Field Entry Marker', 51.709503, -0.364253, false, (SELECT id FROM areas WHERE name='Bricket Wood'), null);
 commit;
